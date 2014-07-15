32a70e34f3e2bc75736c858adf8f1232de1ad0dd
Tom Keays <tomkeays@gmail.com>
2014-07-14 20:32:49 -0400

    modify footer to include food_restrictions link

--- x-footer.php
+++ x-footer.php
@@ -4,7 +4,8 @@
   <td>Czar Reports: </td>
   <td>
     <a href="excel_button_list.php">Button List</a> /
-    <a href="excel_food_restrictions.php">Food Restrictions</a> /
+    <a href="excel_food_restrictions.php">Food Restrictions</a> 
+    (<a href="food_restrictions.php">HTML</a>) /
     <a href="excel_shirt_orders.php">Shirt Orders (Aggregate)</a> /
     <a href="excel_team_shirt_orders.php">Shirt Orders by Team</a> /
     <a href="excel_registration_summary.php">Registration Summary</a>
